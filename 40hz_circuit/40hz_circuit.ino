5cfe6f45721a47befb03ca179c084893914a1e77
antfriend <antfriend@gmail.com>
2024-02-06 17:08:53 -0700

    Update 40hz_circuit.ino

--- 40hz_circuit/40hz_circuit.ino
+++ 40hz_circuit/40hz_circuit.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+// Define the built-in LED pin. On most Arduino boards, this is pin 13.
+const int ledPin = LED_BUILTIN;
 
+void setup() {
+  // Initialize the digital pin as an output.
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Turn the LED on for 3 milliseconds
+  digitalWrite(ledPin, HIGH); // Turn the LED on
+  delay(3); // Wait for 3 milliseconds
 
+  // Then turn the LED off for the remainder of the 25ms cycle
+  // to achieve a 40 Hz blinking frequency
+  digitalWrite(ledPin, LOW); // Turn the LED off
+  delay(22); // Wait for 22 milliseconds
 }
