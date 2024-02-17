c98741066dcb1b713785964bb91cbeb82bb49c19
antfriend <antfriend@gmail.com>
2024-02-17 12:15:26 -0700

    on pin 14

--- 40hz_circuit/40hz_circuit.ino
+++ 40hz_circuit/40hz_circuit.ino
@@ -1,19 +1,31 @@
 // Define the built-in LED pin. On most Arduino boards, this is pin 13.
 const int ledPin = LED_BUILTIN;
+const int led2Pin = 14;
+const int MotorRedPin = 5;
+const int MotorBluePin = 4;
 int loopy = 0;
 
 void setup() {
   // Initialize the digital pin as an output.
   pinMode(ledPin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+//  pinMode(MotorRedPin, OUTPUT);
+//  pinMode(MotorBluePin, OUTPUT);
 }
 
 void loop() {
   // Turn the LED on for 3 milliseconds
   digitalWrite(ledPin, HIGH); // Turn the LED on
+  digitalWrite(led2Pin, HIGH);
+  // digitalWrite(MotorRedPin, HIGH);
+  // digitalWrite(MotorBluePin, LOW);
   delay(3); // Wait for 3 milliseconds
 
   // Then turn the LED off for the remainder of the 25ms cycle
   // to achieve a 40 Hz blinking frequency
   digitalWrite(ledPin, LOW); // Turn the LED off
+  digitalWrite(led2Pin, LOW);
+  // digitalWrite(MotorRedPin, LOW);
+  // digitalWrite(MotorBluePin, LOW);
   delay(22); // Wait for 22 milliseconds
 }
--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 A project for building a physical device that produces a precisely calibrated 40hz light and sound for promoting gamma-wave brain activity. 
 Gamma brain waves activate beta-amyloid elimination, removing the plaques common in Alzheimer's.
 
+GENUS, (Gamma Entrainment Using Sensory Stimuli)   
+[update 2022](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9797689/)
 
-Drivers for 
+
+Drivers for   
+https://a.co/d/bKCClQN
+
 
 https://arduino.stackexchange.com/questions/12977/attiny88-programming-with-arduino
 
@@ -12,3 +17,6 @@ https://github.com/SpenceKonde/ATTinyCore?tab=readme-ov-file
 
 
 https://github.com/SpenceKonde/ATTinyCore/blob/v2.0.0-devThis-is-the-head-submit-PRs-against-this/Installation.md
+
+
+https://news.mit.edu/2023/40-hz-vibrations-reduce-alzheimers-pathology-symptoms-mouse-models-0605
